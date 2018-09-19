b2e492b72aa57bf78c83aaa01102754dd89bd1c4
Adam Morsi <adam.morsi@yahoo.com>
2018-09-19 12:44:27 +0300

    Basics of data binding (until lecture 31)

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,15 +6,32 @@ import {Component, OnInit} from '@angular/core';
     // selector: '[app-servers]',
     // selector by class
     // selector: '.app-servers',
-    template: '<app-server></app-server><app-server></app-server>',
+    // inline HTML code
+    // template: '<app-server></app-server><app-server></app-server>',
+    templateUrl: './servers.component.html',
     styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+    allowNewServer = false;
+    serverCreationStatus = 'No server was created';
+    serverName = '';
 
     constructor() {
+        setTimeout(() => {
+            this.allowNewServer = true;
+        }, 2000);
     }
 
     ngOnInit() {
+    }
+
+    onCreateServer = () => {
+        this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    }
+
+    // The following method will be used for event binding using the input event
+    onUpdateServerName = (event: any) => {
+        this.serverName = event.target.value;
     }
 
 }
